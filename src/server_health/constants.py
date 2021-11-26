55a8bb01d00c6f15b25cdb4b690d4be5ab1b6f22
daudnadeem <d.nadeem93@gmail.com>
2021-11-26 15:08:07 +0000

    removed unnecssary constant

--- src/server_health/constants.py
+++ src/server_health/constants.py
@@ -1,5 +1,4 @@
 # That will never change - so it is okay to hardcode these.
-root_repo_name = "server_health"
 default_logger = "magnificent_logger"
 active = "Magnificent!"
 inactive = "Internal Server Error"
